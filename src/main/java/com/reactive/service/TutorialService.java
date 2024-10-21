191b27c59cb511d3e98a6b75b20af095bf99641b
Nikhil Sharma <sharmanikhilm05@gmail.com>
2024-10-21 10:07:57 +0530

    Added the service class for Tutorial Management

---
+++ src/main/java/com/reactive/service/TutorialService.java
@@ -0,0 +1,53 @@
+package com.reactive.service;
+
+import com.reactive.entity.Tutorial;
+import com.reactive.repository.TutorialRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class TutorialService {
+    private final TutorialRepository tutorialRepository;
+
+    public Flux<Tutorial> findAll() {
+        return tutorialRepository.findAll();
+    }
+
+    public Flux<Tutorial> findByTitleContaining(String title) {
+        return tutorialRepository.findByTitleContaining(title);
+    }
+
+    public Mono<Tutorial> findById(Integer id) {
+        return tutorialRepository.findById(id);
+    }
+
+    public Mono<Tutorial> save(Tutorial tutorial) {
+        return tutorialRepository.save(tutorial);
+    }
+
+    public Mono<Tutorial> update(Integer id, Tutorial tutorial) {
+        return tutorialRepository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("Tutorial not found with " + id)))
+                .flatMap(tutorialSnapshotDb -> {
+                    tutorialSnapshotDb.setDescription(tutorial.getDescription());
+                    tutorialSnapshotDb.setPublished(tutorial.getPublished());
+                    tutorialSnapshotDb.setTitle(tutorial.getTitle());
+                    return tutorialRepository.save(tutorialSnapshotDb);
+                });
+    }
+
+    public Mono<Void> deleteById(Integer id) {
+        return tutorialRepository.deleteById(id);
+    }
+
+    public Mono<Void> deleteAll() {
+        return tutorialRepository.deleteAll();
+    }
+
+    public Flux<Tutorial> findByPublished(boolean isPublished) {
+        return tutorialRepository.findByPublished(isPublished);
+    }
+}
